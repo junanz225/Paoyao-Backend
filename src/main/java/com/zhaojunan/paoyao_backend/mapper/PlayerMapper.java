7f39f44af3f44b73656b263112056658a3042f56
junanzhao <1499563136@qq.com>
2025-11-16 21:42:20 -0800

    fix: resolve mapper/DTO issues

--- src/main/java/com/zhaojunan/paoyao_backend/mapper/PlayerMapper.java
+++ src/main/java/com/zhaojunan/paoyao_backend/mapper/PlayerMapper.java
@@ -3,6 +3,7 @@ package com.zhaojunan.paoyao_backend.mapper;
 import com.zhaojunan.paoyao_backend.model.dto.response.PlayerDTO;
 import com.zhaojunan.paoyao_backend.model.entity.Player;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -10,8 +11,12 @@ public interface PlayerMapper {
 
     PlayerMapper INSTANCE = Mappers.getMapper(PlayerMapper.class);
 
+    @Mapping(source = "id",   target = "playerId")
+    @Mapping(source = "name", target = "playerName")
     PlayerDTO toDTO(Player player);
 
+    @Mapping(source = "playerId",   target = "id")
+    @Mapping(source = "playerName", target = "name")
     Player toEntity(PlayerDTO dto);
 
 }
--- src/main/java/com/zhaojunan/paoyao_backend/model/dto/request/JoinRequest.java
+++ src/main/java/com/zhaojunan/paoyao_backend/model/dto/request/JoinRequest.java
@@ -1,7 +1,9 @@
 package com.zhaojunan.paoyao_backend.model.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Request sent by the frontend when a player wants to join the game room.
@@ -13,6 +15,8 @@ import lombok.Data;
  */
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class JoinRequest {
 
     private String type; // "join"
